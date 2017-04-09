43d199bb52c3eb522f6fb389e254e656045452e0
Gadir Rustamli <qedir_rustemli@hotmail.com>
2017-04-10 02:46:20 +0400

    fixed signal error

--- bili_api/models/search_alert.py
+++ bili_api/models/search_alert.py
@@ -5,5 +5,7 @@ class SearchAlert(models.Model):
     search_term     = models.CharField(max_length=150)
     owner           = models.ForeignKey(User, related_name="search_alert")
     notify          = models.BooleanField(default=True)
+    def __str__(self):
+        return self.search_term
     class Meta:
         unique_together = ('owner', 'search_term')
