ec8f8e26deeda24de8776736b522843934c9325d
Gadir Rustamli <qedir_rustemli@hotmail.com>
2017-04-10 02:10:10 +0400

    update search-alert model

--- bili_api/models/search_alert.py
+++ bili_api/models/search_alert.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class SearchAlert(models.Model):
-    search_term     = models.TextField()
+    search_term     = models.CharField(max_length=150)
     owner           = models.ForeignKey(User, related_name="search_alert")
     notify          = models.BooleanField(default=True)
+    class Meta:
+        unique_together = ('owner', 'search_term')
