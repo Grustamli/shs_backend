4a17cfb453112a61daac54bb43f33aecbcaba86e
Gadir Rustamli <qedir_rustemli@hotmail.com>
2017-04-11 01:51:10 +0400

    changed search alert view

--- bili_api/views/search_alert.py
+++ bili_api/views/search_alert.py
@@ -6,9 +6,12 @@ class SearchAlertListView(ListCreateAPIView):
     serializer_class = SearchAlertListSerializer
     def get_queryset(self, *args, **kwargs):
         queryset = SearchAlert.objects.all()
-        user = self.request.query_params.get('user')
+        user = self.request.query_params.get('user', None)
+        search_term = self.request.query_params.get('search-term', None)
         if user is not None:
             queryset = queryset.filter(owner__username=user)
+        if search_term is not None:
+            queryset = queryset.filter(search_term=search_term)
         return queryset
     def perform_create(self, serializer):
         user = self.request.user
